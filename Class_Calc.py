b6c73bff0c15188c99d9c5c8f0c4955d499bb53f
KaninWithRice <mishalnorona014@gmail.com>
2023-05-22 13:37:55 +0800

    Addition

--- Class_Calc.py
+++ Class_Calc.py
@@ -1,6 +1,10 @@
 # Mohammad Mishal S. Noroña | BSCPE 1-5 | Assignment #7
 # Create a Class Calculator
+class Calculator:
 # Addition
+    def addition(self, num1, num2):
+        add = num1 + num2
+        return add
 # Subtraction
 # Multiplication
 # Division
