7a70a6c3d36fcc8b834cb0d2134562592508de9d
KaninWithRice <mishalnorona014@gmail.com>
2023-05-23 02:42:45 +0800

    Working Calculator

--- Calculator.py
+++ Calculator.py
@@ -8,3 +8,14 @@ num1 = ui.num_input()
 num2 = ui.num_input()
 
 operation = ui.operation_input()
+if operation == "a":
+    print(num1, "+", num2, "=", calc.addition(num1, num2))
+
+elif operation == "s":
+    print(num1, "-", num2, "=", calc.subtraction(num1, num2))
+
+elif operation == "m":
+    print(num1, "*", num2, "=", calc.multiplication(num1, num2))
+
+elif operation == "d":
+    print(num1, "/", num2, "=", calc.division(num1, num2))
