9ab8cd8d10d16c17de12fa54cd65fc4720a9a5b5
KaninWithRice <mishalnorona014@gmail.com>
2023-05-22 14:52:36 +0800

    UserInput

---
+++ User_Input.py
@@ -0,0 +1,7 @@
+class UserInput:
+    def num1_input(self):
+        num1 = float(input("Enter the first number:"))
+        return num1
+    def num2_input(self):
+        num2 = float(input("Enter the second number:"))
+        return num2
