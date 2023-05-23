fc627493e57d7058ab69ccc8bfaa80e12eb83a63
KaninWithRice <mishalnorona014@gmail.com>
2023-05-23 17:05:57 +0800

    Time Delay

--- Calculator.py
+++ Calculator.py
@@ -11,17 +11,27 @@ def start():
     num2 = ui.num_input()
     # Ask for Operation Input
     operation = ui.operation_input()
+    # Add Time Delay
+    import time
     if operation == "a":
-        print(num1, "+", num2, "=", calc.addition(num1, num2))
+        print("\nCalculating Please wait......\n")
+        time.sleep(1.0)
+        print(num1, "+", num2, "\nAnswer =", calc.addition(num1, num2))
         # If user enters "a" Print output of addition
     elif operation == "s":
-        print(num1, "-", num2, "=", calc.subtraction(num1, num2))
+        print("\nCalculating Please wait......\n")
+        time.sleep(1.0)
+        print(num1, "-", num2, "\nAnswer =", calc.subtraction(num1, num2))
         # If user enters "s" Print output of subtraction
     elif operation == "m":
-        print(num1, "*", num2, "=", calc.multiplication(num1, num2))
+        print("\nCalculating Please wait......\n")
+        time.sleep(1.0)
+        print(num1, "*", num2, "\nAnswer =", calc.multiplication(num1, num2))
         # If user enters "m" Print output of multiplication
     elif operation == "d":
-        print(num1, "/", num2, "=", calc.division(num1, num2))
+        print("\nCalculating Please wait......\n")
+        time.sleep(1.0)
+        print(num1, "/", num2, "\nAnswer =", calc.division(num1, num2))
         # If user enters "d" Print output of division
 
     # Ask for another Input
